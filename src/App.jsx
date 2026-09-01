289eb7a8ba4c3a393b03b860ead64515d654de52
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-01 14:49:01 +0100

    feat: render multiple category rows in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Header from "./components/header/Header";
 import Banner from "./components/Banner/Banner";
 import DisplayRow from "./components/DisplayRow/DisplayRow";
-
-
+import {
+  trendingNow,
+  actionAndAdventure,
+  newReleases,
+  continueWatching,
+  netflixOriginals,
+} from "./Data/Data";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
-      <DisplayRow />
+      <DisplayRow title="Trending Now" movies={trendingNow} isFirst />
+      <DisplayRow title="Action & Adventure" movies={actionAndAdventure} />
+      <DisplayRow title="New Releases" movies={newReleases} />
+      <DisplayRow title="Continue Watching" movies={continueWatching} />
+      <DisplayRow title="Netflix Originals" movies={netflixOriginals} />
     </>
   );
 }
