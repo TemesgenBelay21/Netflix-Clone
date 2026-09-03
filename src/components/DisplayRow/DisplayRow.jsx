552e7e0858ed085b6afacae03a663d9153100cc5
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-03 11:30:36 +0100

    types: add JSDoc prop types to DisplayRow

--- src/components/DisplayRow/DisplayRow.jsx
+++ src/components/DisplayRow/DisplayRow.jsx
@@ -1,6 +1,10 @@
 import styles from "./DisplayRow.module.css";
 import SlideShow from "../SlideShow/SlideShow";
 
+/**
+ * A single horizontal row of movies.
+ * @param {{ title: string, movies: import("../../Data/Data.js").Movie[] }} props
+ */
 function DisplayRow({ title, movies }) {
   return (
     <div className={styles.mainWrapper}>
