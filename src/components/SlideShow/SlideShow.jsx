a8fd7881fc310ee3d382ed0c362e90eb231d4170
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-03 11:30:57 +0100

    types: add JSDoc prop types and clean unused import in SlideShow

--- src/components/SlideShow/SlideShow.jsx
+++ src/components/SlideShow/SlideShow.jsx
@@ -1,38 +1,27 @@
-import React from 'react'
-
-// styling
 import styles from './SlideShow.module.css'
+import MovieCard from './../MovieCard/MovieCard'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { Navigation } from 'swiper/modules'
 
-import MovieCard from './../MovieCard/MovieCard';
-
-// import swiper and its components
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import {Navigation} from "swiper/modules"
-
-
-function SlideShow({title,movies}) {
+/**
+ * A horizontally scrolling carousel of movie cards.
+ * @param {{ title: string, movies: import("../../Data/Data.js").Movie[] }} props
+ */
+function SlideShow({ title, movies }) {
   return (
-    
     <div>
-      <h2 className={styles.title}   >{title}</h2>
-      <div className={styles.Row}>   
-        <Swiper modules={[Navigation]}
-          navigation
-          spaceBetween={10}
-        slidesPerView={5.8}>    
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.Row}>
+        <Swiper modules={[Navigation]} navigation spaceBetween={10} slidesPerView={5.8}>
           {movies?.map((movie) => (
-          <SwiperSlide key={movie.id}>
+            <SwiperSlide key={movie.id}>
               <MovieCard movie={movie} />
             </SwiperSlide>
-        ))}
-      </Swiper>
+          ))}
+        </Swiper>
       </div>
-     
     </div>
   )
 }
