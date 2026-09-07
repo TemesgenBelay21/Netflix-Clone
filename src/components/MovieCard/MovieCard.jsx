e06eacbfb9b59c929bd899439c7a4a3203f5300f
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-07 19:38:14 +0100

    feat: rebuild card with portrait poster and expandable info panel

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,77 +1,66 @@
 import styles from "./MovieCard.module.css";
-import { FaCirclePlay } from "react-icons/fa6";
-import { BsPlusCircle } from "react-icons/bs";
-import { GoCheckCircleFill } from "react-icons/go";
-import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { BsPlayFill, BsPlusLg } from "react-icons/bs";
+import { IoIosThumbsUp, IoIosArrowDown } from "react-icons/io";
 
 /**
- * A movie card with a hover-to-expand overlay.
+ * A Netflix-style portrait card. Hovering expands the card and
+ * drops an info panel below the poster with actions and metadata.
  * @param {{ movie: import("../../Data/Data.js").Movie }} props
  */
 function MovieCard({ movie }) {
   const genres = movie?.genres || [];
-  return (
-    <div className={styles.cardwrapper}>
-      {/* poster image */}
-      <img
-        className={styles.poster}
-        src={movie?.poster_path}
-        alt={movie?.title}
-      />
 
-      {/* hover card */}
-      <div className={styles.hoverCard}>
-        {/* img */}
+  return (
+    <div className={styles.card}>
+      <div className={styles.posterWrap}>
         <img
-          className={styles.hoverImage}
+          className={styles.poster}
           src={movie?.poster_path}
           alt={movie?.title}
+          draggable={false}
         />
-        {/* badge */}
-        <div className={styles.badge}>{movie?.badge}</div>
+        {movie?.badge && <span className={styles.badge}>{movie.badge}</span>}
+      </div>
 
-        {/* button row */}
+      <div className={styles.info}>
         <div className={styles.buttonsRow}>
-          <FaCirclePlay
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <BsPlusCircle
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <GoCheckCircleFill
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <IoIosArrowDropdownCircle
-            className={styles.circleButtonSmall}
-            color="white"
-            size={40}
-          />
+          <button
+            type="button"
+            aria-label="Play"
+            className={`${styles.roundBtn} ${styles.playBtn}`}
+          >
+            <BsPlayFill />
+          </button>
+          <button type="button" aria-label="Add to My List" className={styles.roundBtn}>
+            <BsPlusLg />
+          </button>
+          <button type="button" aria-label="Like" className={styles.roundBtn}>
+            <IoIosThumbsUp />
+          </button>
+          <button
+            type="button"
+            aria-label="More info"
+            className={`${styles.roundBtn} ${styles.moreBtn}`}
+          >
+            <IoIosArrowDown />
+          </button>
         </div>
 
-        {/* meta data row */}
+        <p className={styles.title}>{movie?.title}</p>
+
         <div className={styles.metaRow}>
+          <span className={styles.match}>97% Match</span>
           <span className={styles.tag}>{movie?.matureRating}</span>
-          <span className={styles.tag}>{movie?.category}</span>
           <span className={styles.tag}>{movie?.quality}</span>
         </div>
-        {/* genres */}
+
         <div className={styles.genres}>
-          {genres?.map((g, index) => {
-            return (
-              <span key={index}>
-                {g}
-                {index < genres.length - 1 && (
-                  <span className={styles.dot}>•</span>
-                )}
-              </span>
-            );
-          })}
+          {genres.map((genre, index) => (
+            <span key={index}>
+              {genre}
+              {index < genres.length - 1 && <span className={styles.dot}>•</span>}
+            </span>
+          ))}
         </div>
       </div>
     </div>
